637c1e8250ed67e2afd2060ea495a83bc678ac18
kaiser tan king sheng <kaisertan@kaisers-MacBook-Air.local>
2026-08-27 23:42:19 +0800

    Add Firebase authentication and Google sign-in

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'screens/login_screen.dart';
 import 'shared/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const HomeboundApp());
 }
 
